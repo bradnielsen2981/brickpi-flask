8b5f91c0c968f778ea9414cffdc7d58cd68c8138
bradnielsen2981 <brad_nielsen29@hotmail.com>
2020-02-28 22:31:46 +0000

    updated brickpiinterface to use IMU sensor

--- brickpiinterface.py
+++ brickpiinterface.py
@@ -6,47 +6,49 @@ import logging
 import threading 
 from di_sensors.inertial_measurement_unit import InertialMeasurementUnit
 from di_sensors.temp_hum_press import TempHumPress
+#DO I NEED TO USE MUTEX???
+
+NOREADING = 3000 #just using 3000 to represent no reading
+MAGNETIC_DECLINATION = 11 #i believe this is correct for Brisbane
 
 #Created a Class to wrap the robot functionality, one of the features is the idea of keeping track of the CurrentCommand, this is important when more than one process is running...
 class Robot():
 
-    #use the init method to define your configuration
-    def __init__(self, log=None):
+    #Initialise log and timelimit
+    def __init__(self, logger=logging.getLogger(__name__), timelimit=30):
+        self.CurrentCommand = "loading"
         self.BP = brickpi3.BrickPi3() # Create an instance of the BrickPi3
-        if log == None:
-            self.logger = logging.getLogger(__name__) #This doesnt seem to work??!!!
-        else:
-            self.logger = log
+        self.timelimit = timelimit
+        self.logger = logger
         bp = self.BP
-
         #---Init Motors--------#
         self.rightmotor = bp.PORT_A
         self.leftmotor = bp.PORT_B
         self.largemotors = bp.PORT_A + bp.PORT_B
         self.mediummotor = bp.PORT_C
+        self.claw_closed = False #current state of the claw
         bp.set_motor_limits(self.mediummotor, 100, 600) #set power and speed limit of the medium motor
-
-        #---Init Sensors--------#
-        #self.thp = TempHumPress() #port is the I2c Grove
-        self.imu = InertialMeasurementUnit()
+        #---Thermal Infrared Sensor Setup--------#
+        self.thermal_thread = None #for later thread
+        self.log("Starting thermal sensor thread to make continual i2c transactions")
         self.thermal = bp.PORT_1
         bp.set_sensor_type(self.thermal, bp.SENSOR_TYPE.I2C, [0, 20]) 
+        self.thermal_thread = threading.Thread(target=self.__update_thermal_sensor_thread, args=(1,))
+        self.thermal_thread.daemon = True
+        self.thermal_thread.start()
+        #---Initialise other Sensors--------#
+        #self.thp = TempHumPress() #port is the I2c Grove
         self.colour = bp.PORT_2 #Colour Sensor
         bp.set_sensor_type(self.colour, bp.SENSOR_TYPE.EV3_COLOR_COLOR)
         #self.gyro = bp.PORT_3 # Lego Gyro Sensor - replaced with IMU sensor
         #bp.set_sensor_type(self.gyro, bp.SENSOR_TYPE.EV3_GYRO_ABS_DPS)
         self.ultra = bp.PORT_4 #UltraSonic Senor
         bp.set_sensor_type(self.ultra, bp.SENSOR_TYPE.EV3_ULTRASONIC_CM)
-        self.thermal_thread = None #for later thread
-        time.sleep(4) #you need to delay or else sensors are not configured!!!'''
+        time.sleep(4)
+        #---Initialise IMU sensor--------#
+        self.imu_status = 0
+        self.imu = InertialMeasurementUnit()
         self.CurrentCommand = "loaded"
-
-        #Start the infrared thermal thread - for some reason when not performing I2c transactions, the infrared thermal sensor disables the motors???
-        self.thermal_thread = threading.Thread(target=self.update_thermal_sensor_thread, args=(1,))
-        self.thermal_thread.daemon = True
-        self.thermal_thread.start()
-        self.logger.info("Starting Thread - thermal sensor i2c transactions")
-        self.logger.info("Configuration Loaded...")
         return
 
     #-----------SENSOR COMMAND----------------#
@@ -54,28 +56,45 @@ class Robot():
     def get_battery(self):
         return self.BP.get_voltage_battery()
 
-    #get the gyro sensor -- returns 0 if not working but total degrees otherwise
-    #can switch between IMU sensor and EV3 sensor using True or False
-    def get_gyro_sensor(self, IMUgyro=True):
+    #get the gyro sensor angle/seconds acceleration from IMU sensor
+    def get_gyro_sensor_IMU(self):
+        bp = self.BP
+        gyro_readings = (NOREADING,NOREADING,NOREADING)
+        try:
+            gyro_readings = self.imu.read_gyroscope() #degrees/s
+            time.sleep(0.01)
+        except Exception as error:
+            self.logger.error("IMU GYRO: " + str(error))
+            self.CurrentCommand = "stop"
+        return gyro_readings
+
+    #gets the temperature using the IMU sensor
+    def get_temperature_IMU(self):
+        bp = self.BP
+        temp = NOREADING
+        try:
+            temp = self.imu.read_temperature()
+            time.sleep(0.01)
+        except Exception as error:
+            self.logger.error("IMU Temp: " + str(error))
+            self.CurrentCommand = "stop"
+        return temp
+
+    #The EV3 gyro sensor sends an absolute rotation value
+    def get_gyro_sensor_EV3(self):
         bp = self.BP
         degrees = 0
         try:
-            if IMUgyro:
-                #the IMU gyroscope actually reads angular velocity
-                #(x,y,z) = self.imu.read_gyroscope()
-                #the IMU euler value actually reads orientation
-                (degrees,roll,pitch) = self.imu.read_euler()
-            else: #using EV3 gyro
-                degrees = bp.get_sensor(self.gyro)[0]
+            degrees = bp.get_sensor(self.gyro)[0]
             time.sleep(0.01)
         except brickpi3.SensorError as error:
-            self.logger.error("GYRO: " + str(error))
+            self.logger.error("EV3 GYRO: " + str(error))
             self.CurrentCommand = "stop"
-        return degrees
+        return degrees    
 
     #get the ultrasonic sensor
     def get_ultra_sensor(self):
-        distance = 0
+        distance = NOREADING
         bp = self.BP
         try:
             distance = bp.get_sensor(self.ultra)
@@ -87,6 +106,7 @@ class Robot():
 
     #read temp and humidity from the I2C port, you need an I2C temp sensor - #disabled currently
     def get_temp_humidity_press_sensor(self):
+        temp, hum, press = (NOREADING,NOREADING,NOREADING)
         try:
             temp = self.thp.get_temperature_celsius()
             hum = self.thp.get_humidity()
@@ -98,29 +118,44 @@ class Robot():
         return(temp,hum,press) #return a tuple containing temp,hum,press'''
 
     #returns the compass value from the IMU sensor - note if the IMU is placed near a motor it can be affected -SEEMS TO RETURN A VALUE BETWEEN -180 and 180. 
-    def get_compass(self):
-        heading = 0
+    def get_compass_IMU(self):
+        heading = NOREADING
         try:
             (x, y, z)  = self.imu.read_magnetometer()
             time.sleep(0.01)
-            heading = (math.atan2(y, x)*(180/math.pi)) + 180 #makes it 0 - 360 degrees
-        except brickpi3.SensorError as error:
+            heading = int(math.atan2(y, x)*(180/math.pi)) + 180 + MAGNETIC_DECLINATION 
+            #make it 0 - 360 degrees
+            if heading > 360:
+                heading -= 360
+        except Exception as error:
             self.logger.error("IMU: " + str(error))
         return heading
 
+    def get_orientation_IMU(self):
+        readings = (NOREADING,NOREADING,NOREADING)
+        try:
+            readings = self.imu.read_euler()
+            time.sleep(0.01)
+        except Exception as error:
+            self.logger.error("IMU Orientation: " + str(error))
+        return readings  
+
     #returns the acceleration from the IMU sensor - could be useful for detecting collisions
-    def get_acceleration_forward(self):
+    def get_acceleration_IMU(self):
+        readings = (NOREADING,NOREADING,NOREADING)
         if self.CurrentCommand != 'stop':
-            (forward,side,updown) = self.imu.read_accelerometer()
-            return forward
-        else:
-            return 0
+            try:
+                readings = self.imu.read_accelerometer()
+            except Exception as error:
+                self.logger.error("IMU Acceleration: " + str(error))    
+            return readings
+        return readings
 
     #returns the colour current sensed - "none", "Black", "Blue", "Green", "Yellow", "Red", "White", "Brown"
     def get_colour_sensor(self):
         bp = self.BP
         value = 0
-        colours = ["none", "Black", "Blue", "Green", "Yellow", "Red", "White", "Brown"]
+        colours = ["NOREADING", "Black", "Blue", "Green", "Yellow", "Red", "White", "Brown"]
         try: 
             value = bp.get_sensor(self.colour) 
             time.sleep(0.01)
@@ -129,7 +164,7 @@ class Robot():
         return colours[value]
 
     #updates the thermal sensor by making continual I2C transactions through a thread
-    def update_thermal_sensor_thread(self, name):
+    def __update_thermal_sensor_thread(self, name):
         while self.CurrentCommand != "exit":
             self.update_thermal_sensor()
         return
@@ -172,7 +207,6 @@ class Robot():
         bp = self.BP
         bp.set_sensor_type(self.thermal, bp.SENSOR_TYPE.NONE) 
         return
-    
 
     #--------------MOTOR COMMANDS-----------------#
     #simply turns motors on
@@ -183,11 +217,11 @@ class Robot():
         return
 
     #moves for the specified time (seconds) and power - use negative power to reverse
-    def move_time_power(self, power, t):
+    def move_power_time(self, power, t):
         bp = self.BP
         self.CurrentCommand = "movepower"
-        target = time.time() + t
-        while time.time() < target and self.CurrentCommand != "stop":
+        timelimit = time.time() + t
+        while time.time() < timelimit and self.CurrentCommand != "stop":
             bp.set_motor_power(self.largemotors, power)
         self.CurrentCommand = "stop"
         self.BP.set_motor_power(self.largemotors, 0)
@@ -201,16 +235,28 @@ class Robot():
         elapsedtime = 0;  start = time.time()
         #ultra sonic sensors can sometimes return 0
         bp.set_motor_power(self.largemotors, power)
-        while (distancedetected > distanceto or distancedetected == 0.0) and (self.CurrentCommand != "stop"):
+        timelimit = time.time() + self.timelimit
+        while ((distancedetected > distanceto or distancedetected == 0.0) and (self.CurrentCommand != "stop") and (time.time() < timelimit)):
             distancedetected = self.get_ultra_sensor()
             self.logger.info("MOVING - Distance detected: " + str(distancedetected))
         self.CurrentCommand = "stop"
         elapsedtime = time.time() - start
         bp.set_motor_power(self.largemotors, 0)
         return elapsedtime
+
+    #Rotate power and time, -power to reverse
+    def rotate_power_time(self, power, t):
+        self.CurrentCommand = "rotate time"
+        bp = self.BP
+        target = time.time() + t
+        while time.time() < target and self.CurrentCommand != 'stop':
+            bp.set_motor_power(self.rightmotor, -power)
+            bp.set_motor_power(self.leftmotor, power)
+        bp.set_motor_power(self.largemotors, 0) #stop
+        return
         
-    #Rotates the robot with power and degrees. negative degrees = left. 
-    def rotate_power_degrees(self, power, degrees):
+    #Rotates robot with power and degrees using EV3 Gyro sensor. Negative degrees = left. 
+    def rotate_power_degrees_EV3(self, power, degrees):
         self.CurrentCommand = "rotate"
         bp = self.BP
         currentdegrees = self.get_gyro_sensor()
@@ -219,7 +265,9 @@ class Robot():
         symbol = '>' if degrees < 0 else '<'  #shorthand if statement
         power = -(power) if degrees < 0 else power
         expression = 'currentdegrees' + symbol + 'targetdegrees'
-        while eval(expression) and self.CurrentCommand != "stop":
+        #assuming of robot rotation for longer than 20seconds there is an error and stop
+        timelimit = time.time() + self.timelimit
+        while eval(expression) and self.CurrentCommand != "stop" and time.time() < timelimit:
             bp.set_motor_power(self.rightmotor, -power)
             bp.set_motor_power(self.leftmotor, power)
             self.log("Gyro degrees remaining: " + str(targetdegrees - currentdegrees))
@@ -227,12 +275,45 @@ class Robot():
         self.CurrentCommand = "stop"
         bp.set_motor_power(self.largemotors, 0) #stop
         return
+        
+    #Rotoates the robot with power and degrees using the IMU sensor. Negative degrees = left.
+    def rotate_power_degrees_IMU(self, power, degrees):
+        self.CurrentCommand = "rotate"
+        bp = self.BP
+        marginoferror = 0
+        symbol = '<'; limit = 0
+        if degrees == 0:
+            return
+        elif degrees < 0:
+            symbol = '>='; limit = degrees+marginoferror; 
+        else:
+            symbol = '<='; limit = degrees-marginoferror; power = -power
+        totaldegreesrotated = 0; lastrun = 0
+        timelimit = time.time() + self.timelimit #useful if time is exceeded
+        print("target degrees: " + str(degrees))
+        print(str(totaldegreesrotated) + str(symbol) + str(limit))
+        while eval("totaldegreesrotated" + str(symbol) + "limit") and (self.CurrentCommand != "stop") and (time.time() < timelimit):
 
-    #rotates the robot until faces targetheading
+            #need to modulate loop frequency
+
+            lastrun = time.time()
+            bp.set_motor_power(self.rightmotor, power)
+            bp.set_motor_power(self.leftmotor, -power)
+            print("Total degrees rotated: " + str(totaldegreesrotated))
+            totaldegreesrotated += (time.time() - lastrun)*self.get_gyro_sensor_IMU()[1] #y-axis
+        self.CurrentCommand = "stop"
+        bp.set_motor_power(self.largemotors, 0) #stop
+        return
+
+    #rotates the robot until faces targetheading - only works for a heading between 0 - 360
     def rotate_power_heading(self, power, targetheading):
         bp = self.BP
         self.CurrentCommand = "rotate to heading"
-        heading = self.get_compass()
+        if targetheading < 0:
+            targetheading += 360
+        elif targetheading > 360:
+            targetheading -= 360
+        heading = self.get_compass_sensor()
         if heading == targetheading:
             return
         symbol = '<'; limit = 0
@@ -241,17 +322,18 @@ class Robot():
         else:
             symbol = '>='; limit = targetheading+5; power = -power
         expression = 'heading' + symbol + 'limit'
-        while eval(expression) and self.CurrentCommand != "stop":
+        timelimit = time.time() + self.timelimit
+        while ((eval(expression) and (self.CurrentCommand != "stop") and time.time() < timelimit)):
             bp.set_motor_power(self.rightmotor, -power)
             bp.set_motor_power(self.leftmotor, power)
             self.log("Current heading: " + str(heading))
-            heading = self.get_compass()
+            heading = self.get_compass_sensor()
         self.CurrentCommand = "stop"
         bp.set_motor_power(self.largemotors, 0) #stop
         return
 
     #moves the target class to the target degrees
-    def move_claw_targetdegrees(self, degrees):
+    def __move_claw_targetdegrees(self, degrees):
         self.CurrentCommand = "move claw"
         degrees = -degrees #reversing 
         bp = self.BP
@@ -266,7 +348,9 @@ class Robot():
             symbol = '>'; limit = degrees + 5
         expression = 'currentdegrees' + symbol + 'limit'
         currentdegrees = bp.get_motor_encoder(self.mediummotor)
-        while eval(expression) and self.CurrentCommand != "stop":
+
+        timelimit = time.time() + self.timelimit/2
+        while (eval(expression) and (self.CurrentCommand != "stop") and (time.time() < timelimit)):
             currentdegrees = bp.get_motor_encoder(self.mediummotor) #where is the current angle
             bp.set_motor_position(self.mediummotor, degrees)
             currentdegrees = bp.get_motor_encoder(self.mediummotor) #ACCURACY PROBLEM
@@ -276,17 +360,34 @@ class Robot():
 
     #open the claw
     def open_claw(self):
-        robot.move_claw_targetdegrees(-1000)
+        if self.claw_closed == True:
+            self.__move_claw_targetdegrees(-1100)
+            self.claw_closed = False
         return
 
     #close the claw
     def close_claw(self):
-        robot.move_claw_targetdegrees(1000)   
+        if self.claw_closed == False:
+            self.__move_claw_targetdegrees(1100)
+            self.claw_closed = True   
         return
 
-    #log out whatever !!!!!THIS IS NOT WORKING UNLESS FLASK LOG USED DONT KNOW WHY!!!!!
+    #log out whatever !!!!!THIS IS NOT WORKING UNLESS FLASK LOG USED, DONT KNOW WHY!!!!!
     def log(self, message):
         self.logger.info(message)
+        return
+
+    #print out a complete output from the IMU sensor
+    def test_calibrate_imu(self):
+        timelimit = time.time() + 10 #maximum of 20 seconds to calibrate compass sensor
+        print("Move around the robot to calibrate the Compass Sensor...")
+        self.imu_status = 0
+        while self.imu_status != 3 and time.time() < timelimit:
+            try:
+                self.imu_status = self.imu.BNO055.get_calibration_status()[3]
+            except Exception as error:
+                print("IMU Calibration Error: " + error)
+        print("IMU Compass Sensor has been calibrated")
         return
 
     #stop all motors and set command to stop
@@ -294,7 +395,7 @@ class Robot():
         bp = self.BP
         bp.set_motor_power(self.largemotors+self.mediummotor, 0)
         self.CurrentCommand = "stop"
-        return
+        return      
 
     #returns the current command
     def get_current_command(self):
@@ -305,11 +406,13 @@ class Robot():
         sensordict = {} #create a dictionary for the sensors
         sensordict['battery'] = self.get_battery()
         sensordict['colour'] = self.get_colour_sensor()
-        sensordict['ultra'] = self.get_ultra_sensor()
+        sensordict['ultrasonic'] = self.get_ultra_sensor()
         sensordict['thermal'] = self.get_thermal_sensor()
-        sensordict['accel'] = self.get_acceleration_forward()
-        sensordict['compass'] = self.get_compass()
-        sensordict['gyro'] = self.get_gyro_sensor()
+        sensordict['acceleration'] = self.get_acceleration_IMU()
+        sensordict['compass'] = self.get_compass_IMU()
+        sensordict['gyro'] = self.get_gyro_sensor_IMU()
+        sensordict['temperature'] = self.get_temperature_IMU()
+        sensordict['orientation'] = self.get_orientation_IMU()
         #to do need to get temperature from IMU sensor
         return sensordict
 
@@ -326,24 +429,17 @@ class Robot():
 #--------------------------------------------------------------------
 #Only execute if this is the main file, good for testing code
 if __name__ == '__main__':
-    robot = Robot()
-    #robot.safe_exit()
-    robot.close_claw()
-    robot.open_claw()
-    #robot.rotate_power_degrees(30,90)
+    robot = Robot(timelimit=10)
+    print(robot.get_all_sensors())
+    #robot.rotate_power_degrees_IMU(20,90)
     #robot.move_power_untildistanceto(30,10)
-    robot.rotate_power_heading(20,180)
-
-    target = time.time() + 2
-    while time.time() < target:
-        print("Battery: " + str(robot.get_battery()))
-        print("Compass: " + str(robot.get_compass()))
-        print("Gyro: " + str(robot.get_gyro_sensor()))
-        print("Colour: " + str(robot.get_colour_sensor()))
-        print("Ultra: " + str(robot.get_ultra_sensor()))
-        print("Thermal: " + str(robot.get_thermal_sensor()))
-        print("Accelerometer:" + str(robot.get_acceleration_forward()))
-    robot.CurrentCommand = "stop" 
+    #robot.move_power_time(40,1)
+    #robot.test_calibrate_imu()
+    #robot.rotate_power_time(30, 3)
+    #robot.close_claw()
+    #robot.rotate_power_heading(20,380)
+    #robot.safe_exit()
+    #robot.CurrentCommand = "stop" 
     robot.safe_exit()
-    #robot.disable_thermal_sensor() -- could also enable and disable thermal sensor when needed
+    #robot.disable_thermal_sensor() -- could also enable and disable thermal sensor when needed'''
 
--- static/getallstats.js
+++ static/getallstats.js
@@ -1,5 +1,5 @@
 var recurringhandle = null;  //can be used to delete recurring function if you want
-recurringhandle = setInterval(get_all_stats, 3000);
+recurringhandle = setInterval(get_all_stats, 1000);
 
 
 //THis recurring function gets data using JSON
@@ -7,7 +7,16 @@ function get_all_stats() {
     JSONrequest('/getallstats','POST', draw_stats); //Once data is received it is passed to the drawchart function
 }
 
-//with received json data, draw the chart
+//with received json data, draw the chart using tempate literals ${results.battery}
+//results contains all the variables: battery, compass etc
 function draw_stats(results) {
-    console.log(results);
+    document.getElementById('sbattery').innerHTML = String(results.battery);
+    document.getElementById('scolour').innerHTML = String(results.colour);
+    document.getElementById('sultrasonic').innerHTML = String(results.ultrasonic);
+    document.getElementById('sthermal').innerHTML = String(results.thermal);
+    document.getElementById('sacceleration').innerHTML =String(results.acceleration);
+    document.getElementById('scompass').innerHTML = String(results.scompass);
+    document.getElementById('sgyro').innerHTML = String(results.sgyro);
+    document.getElementById('stemperature').innerHTML = String(results.temperature);
+    document.getElementById('sorientation').innerHTML = String(results.orientation);
 }
